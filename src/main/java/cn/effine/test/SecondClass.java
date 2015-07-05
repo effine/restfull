0d5f1eaf866f8b3390973f6d04aac370f289a790
effine <verphen@163.com>
2015-07-05 23:02:12 +0800

    update

---
+++ src/main/java/cn/effine/test/SecondClass.java
@@ -0,0 +1,17 @@
+/**
+ * @Date 2014年11月20日  下午6:06:42
+ * @author 张亚飞
+ * @Copyright 云路科技  ©2014
+ */
+
+package cn.effine.test;
+
+public class SecondClass {
+	public void sayHello() {
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("------");
+	}
+}
