9e29db5a5a90ba9c8f3ab21e07ae46f56a45e6af
effine <verphen@163.com>
2015-07-16 21:33:21 +0800

    update

---
+++ src/main/java/cn/effine/test/FourClass.java
@@ -0,0 +1,20 @@
+/**
+ * @Date 2014年11月20日  下午6:06:42
+ * @author 张亚飞
+ * @Copyright 云路科技  ©2014
+ */
+
+package cn.effine.test;
+
+public class FourClass {
+	public static void main(String[] args) {
+		
+		System.err.println();
+		System.err.println();
+		System.err.println();
+		System.err.println();
+		System.err.println();		
+		
+		
+	}
+}
