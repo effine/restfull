1ec651066a7d8be74ae6cc0a2572e0328b6a83be
effine-mac <verphen@163.com>
2015-11-17 23:49:52 +0800

    optimization code

--- src/main/java/cn/effine/rest/support/GenericRestResponse.java
+++ src/main/java/cn/effine/rest/support/GenericRestResponse.java
@@ -13,35 +13,35 @@ import cn.effine.web.Response;
  */
 public class GenericRestResponse implements IRestResponse {
 
-    private HttpServletResponse response;
-    private IRestService restService;
-    private Response responseData; //返回的JSON 结果
+	private HttpServletResponse response;
+	private IRestService restService;
+	private Response responseData; // 返回的JSON 结果
 
-    public Response getResponseData() {
-        return responseData;
-    }
+	public Response getResponseData() {
+		return responseData;
+	}
 
-    public void setResponseData(Response responseData) {
-        this.responseData = responseData;
-    }
+	public void setResponseData(Response responseData) {
+		this.responseData = responseData;
+	}
 
-    public GenericRestResponse(HttpServletResponse response) {
-        this.response = response;
-    }
+	public GenericRestResponse(HttpServletResponse response) {
+		this.response = response;
+	}
 
-    public HttpServletResponse getHttpResponse() {
-        return response;
-    }
+	public HttpServletResponse getHttpResponse() {
+		return response;
+	}
 
-    public IRestService getRestService() {
-        return restService;
-    }
+	public IRestService getRestService() {
+		return restService;
+	}
 
-    public void setRestService(IRestService restService) {
-        this.restService = restService;
-    }
+	public void setRestService(IRestService restService) {
+		this.restService = restService;
+	}
 
-    public void writeReponse(Response response) throws RestException {
-        this.responseData = response;
-    }
+	public void writeReponse(Response response) throws RestException {
+		this.responseData = response;
+	}
 }
